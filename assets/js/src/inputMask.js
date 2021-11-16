b4f8b1fcaf96326ba57381f79a9dbf7fd6ff19c5
AlexanderMehighlove <armsof.theocean@mail.ru>
2021-11-16 10:04:48 +0300

    Комплексные правки сайта.

--- assets/js/src/inputMask.js
+++ assets/js/src/inputMask.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    $('body').on('input', '.header__wrapper__input', function(){
+    $('body').on('input', '.main__input', function(){
         this.value = this.value.replace(/[^0-9\^а-яё\s]/g, '');
     });
+    $(".phone").mask("+7 (999) 99-99-999");
 });
